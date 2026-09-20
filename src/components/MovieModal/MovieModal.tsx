60a7239492f697ed2acedcc41629762b655f832f
Oleksandr <alexsandrolegov@gmail.com>
2026-09-20 16:32:53 +0200

    fix: add release date and rating to MovieModal

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -55,6 +55,12 @@ export default function MovieModal({ movie, onClose }: MovieModalProps) {
         <div className={css.info}>
           <h2>{movie.title}</h2>
           <p>{movie.overview}</p>
+          <p>
+            <strong>Release Date:</strong> {movie.release_date}
+          </p>
+          <p>
+            <strong>Rating:</strong> {movie.vote_average} / 10
+          </p>
         </div>
       </div>
     </div>
